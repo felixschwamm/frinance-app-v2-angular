d46575d060d67c4c7d9ce5e7d0b698ef7e712cbe
Felix <felix.schwaemmle@gmail.com>
2024-02-06 14:43:15 +0100

    changed backend to aws (faster)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_BASE_URL: 'https://fbe.schwaemmle.cloud',
+    API_BASE_URL: 'https://237ow9lljj.execute-api.eu-central-1.amazonaws.com',
     ENABLE_FAKE_FETCH: false
 }
